094c67e3d3ca3acb11b1605f1e6503031a99a233
mayard689 <doc-albert@laposte.net>
2020-05-18 09:00:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Lecteur, imagine le titre que tu aimerai voir écrit içi</title>
+    <!-- On peut avoir d'autres méta-données ici -->
+</head>
+<body>
+<h1> Ceci est une page vide executant un script. </h1>
+
+<script type="text/javascript" src="script.js"> </script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,5 @@
+let name="La folle journée de Ferris Bueller";
+let year=1986;
+let director='John Hughes';
+
+alert(name+', sortie en '+year+' et réalisé par '+director);
